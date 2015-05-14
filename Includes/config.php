9bd015c8d075e46d771e61203b61c3c46afe7c06
Christoph <hauer.christoph@live.at>
2015-05-14 03:25:28 +0200

    Webconfig 3

--- Includes/config.php
+++ Includes/config.php
@@ -1,9 +1,9 @@
 <?php
-    define('DB_NAME', 'FHPHPTEST');
-    define('DB_USER', 'b3b4e663edf659');
-    define('DB_PASSWORD', '2e216660');
-    define('DB_HOST', 'eu-cdbr-azure-west-a.cloudapp.net');
+    define('DB_NAME', 'SecureFileManagerDB');
+    define('DB_USER', 'FHProjectsAdmin');
+    define('DB_PASSWORD', 'qwerASDF12');
+    define('DB_HOST', 'vlpd8topfp.database.windows.net');
 
-    define('DEFAULT_ADMIN_USERNAME', 'admin');
-    define('DEFAULT_ADMIN_PASSWORD', 'password');
+    define('DEFAULT_ADMIN_USERNAME', 'FHSecureFileAdmin');
+    define('DEFAULT_ADMIN_PASSWORD', 'qwerASDF12');
 ?>
