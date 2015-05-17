a47eb38ca85572fd3be0fa7e45d311cd9f60a1d0
MaHaller <77748@fhwn.ac.at>
2015-05-17 16:03:54 +0200

    File upload

--- views/Files/index.php
+++ views/Files/index.php
@@ -1,7 +1,7 @@
 <!--=== Breadcrumbs ===-->
 <div class="breadcrumbs">
     <div class="container">
-        <h1 class="pull-left">File Upload</h1>
+        <h1 class="pull-left">Upload List</h1>
         <ul class="pull-right breadcrumb">
             <li><a href="index.html">Home</a></li>
             <li><a href="">Pages</a></li>
@@ -15,12 +15,8 @@
 <div class="container content">
     <div class="row">
         <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
-            <form class="reg-page" action="files/index">
-                <div class="reg-header">
-                    <h2>Upload List</h2>
-                </div>
+
                 TEST!!!!!
-            </form>
         </div>
     </div><!--/row-->
 </div><!--/container-->
