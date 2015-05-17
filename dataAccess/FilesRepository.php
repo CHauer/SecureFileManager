7db03714669ece89b4bf1215bc4f32ac8f944a60
MaHaller <77748@fhwn.ac.at>
2015-05-17 15:05:58 +0200

    File upload

---
+++ dataAccess/FilesRepository.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Christoph
+ * Date: 14.05.2015
+ * Time: 04:02
+ */
+
+class UserRepository{
+
+    /**
+     * @param UserFile $file
+     * @return bool
+     */
+    public function InsertFile(UserFile $file){
+        global $db;
+       /* $stmt = $db->prepare('INSERT INTO [dbo].[User]
+           ([Username]
+           ,[Password]
+           ,[Birthdate]
+           ,[EMail]
+           ,[Description]
+           ,[PictureLink]
+           ,[LockoutEnabled]
+           ,[LockoutEndDate]
+           ,[AccessFailedCount]
+           ,[RoleId]
+           ,[AuthTokenId]
+           ,[Firstname]
+           ,[Lastname])
+     VALUES
+           (:Username,
+           :Password,
+           :Birthdate,
+           :EMail,
+           :Description,
+           :PictureLink,
+           :LockoutEnabled,
+           :LockoutEndDate,
+           :AccessFailedCount,
+           :RoleId,
+           :AuthTokenId,
+           :Firstname,
+           :Lastname)');
+        $stmt->bindParam(":Username", $user->Username);
+        $stmt->bindParam(":Password", $user->Password);
+        $stmt->bindParam(":Birthdate", $user->Birthdate);
+        $stmt->bindParam(":EMail", $user->EMail);
+        $stmt->bindParam(":Description", $user->Description);
+        $stmt->bindParam(":PictureLink", $user->PictureLink);
+        $stmt->bindParam(":LockoutEnabled", $user->LockoutEnabled);
+        $stmt->bindParam(":LockoutEndDate", $user->LockoutEndDate);
+        $stmt->bindParam(":AccessFailedCount", $user->AccessFailedCount);
+        $stmt->bindParam(":RoleId", $user->RoleId);
+        $stmt->bindParam(":AuthTokenId", $user->AuthTokenId);
+        $stmt->bindParam(":Firstname", $user->Firstname);
+        $stmt->bindParam(":Lastname", $user->Lastname);
+
+        $stmt->execute();
+
+        return $stmt->columnCount() == 1;*/
+        return true;
+    }
+
+}
