774a5f371637123d985b6796b886bb215f6af76a
MaHaller <77748@fhwn.ac.at>
2015-05-17 17:18:17 +0200

    File upload

--- views/Files/index.php
+++ views/Files/index.php
@@ -14,9 +14,49 @@
 <!--=== Content Part ===-->
 <div class="container content">
     <div class="row">
-        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+        <div class="job-img margin-bottom-30">
+            <div class="job-img-inputs">
+                <div class="container">
+                    <!-- Serach -->
+                    <div class="row">
+                        <div class="col-sm-4 md-margin-bottom-10">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-tag"></i></span>
+                                <input type="text" placeholder="which user you are looking for" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <button type="button" class="btn-u btn-block btn-u-dark"> Search User</button>
+                        </div>
+                    </div>
 
-                TEST!!!!!
+                    <!-- Show -->
+                    <div class="row">
+                        <div class="row margin-bottom-5">
+                            <div class="col-sm-4 result-category">
+                                <h2>Files</h2>
+                            </div>
+                            <div class="col-sm-8">
+                                <ul class="list-inline clear-both">
+                                    <li class="sort-list-btn">
+                                        <h3>Show :</h3>
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                                                20 <span class="caret"></span>
+                                            </button>
+                                            <ul class="dropdown-menu" role="menu">
+                                                <li><a href="#">All</a></li>
+                                                <li><a href="#">10</a></li>
+                                                <li><a href="#">5</a></li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div><!--/row-->
 </div><!--/container-->
