8728b578dcadedb475d9d2efe645906b9e258acd
doleeb <dominik.leeb@gmx.at>
2015-05-22 01:34:40 +0200

    Files fürs Forum hinzugefügt

---
+++ controllers/forum.php
@@ -0,0 +1,29 @@
+<?php
+/* 
+ * Project: Nathan MVC
+ * File: /controllers/home.php
+ * Purpose: controller for the home of the app.
+ * Author: Nathan Davison
+ */
+
+class ForumController extends BaseController
+{
+    //add to the parent constructor
+    public function __construct($action, $urlValues) {
+        parent::__construct($action, $urlValues);
+
+        //create the model object
+        require("models/forum.php");
+        $this->model = new ForumModel();
+    }
+
+    //default method
+    protected function index()
+    {
+        ConfirmUserIsLoggedOn();
+
+        $this->view->output($this->model->index());
+    }
+}
+
+?>
