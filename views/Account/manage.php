9992b002fdbf2dc1de8bb92b6ccb1a6d55b8c794
Christoph Hauer <hauer.christoph@live.at>
2015-05-22 19:36:08 +0200

    AuthToken

--- views/Account/manage.php
+++ views/Account/manage.php
@@ -13,29 +13,29 @@
 <!--=== Content Part ===-->
 <div class="container content">
     <div class="row" style="height: 500px;">
-        <div class="col-md-8">
+        <div class="col-md-12">
 
             <div class="reg-header">
                 <h2>Welcome <? echo $viewModel->get("fullname") ?> to your Account Management Portal!</h2>
             </div>
 
-            <div class="row">
+            <div class="row margin-bottom-20">
                 <a class="btn btn-default col-md-5" href="/account/editprofile" >Edit Profile</a>
                 <p class="col-md-7" >Here you can edit your Profile, Username, Password, Role and Description.</p>
             </div>
 
-            <div class="row">
+            <div class="row margin-bottom-20">
                 <a class="btn btn-default col-md-5" href="/account/changepassword" >Change User Picture</a>
                 <p class="col-md-7" >Here you can edit your Profile Picture.</p>
             </div>
 
 
-            <div class="row">
+            <div class="row margin-bottom-20">
                 <a class="btn btn-default col-md-5" href="/account/changepassword" >Change Password</a>
                 <p class="col-md-7" >Here you can edit your Password.</p>
             </div>
 
-            <div class="row">
+            <div class="row margin-bottom-20">
                 <a class="btn btn-danger col-md-5" href="/account/deactivate">Deactivate your Account</a>
                 <p class="col-md-7" >Here you can deactivate your Account. The Account is deactivated until the next login.</p>
             </div>
