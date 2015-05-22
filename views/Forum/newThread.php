2ec14210cec350d9e5afff93fab0f48631b6bc94
doleeb <dominik.leeb@gmx.at>
2015-05-22 02:16:15 +0200

    Form für neuen Thread erstellt

--- views/Forum/newThread.php
+++ views/Forum/newThread.php
@@ -1,4 +1,21 @@
 <div class="container content">
-    <h1>New forum thread:</h1>
-    <p>TODO: Form</p>
+    <div class="panel panel-blue margin-bottom-40">
+        <div class="panel-heading">
+            <h3 class="panel-title"><i class="fa fa-tasks"></i> Post a new thread</h3>
+        </div>
+        <div class="panel-body">
+            <form class="margin-bottom-40" role="form">
+                <div class="form-group">
+                    <label for="">Title</label>
+                    <input class="form-control" id="inputTitle" placeholder="Enter title">
+                </div>
+                <div class="form-group">
+                    <label for="inputDescription">Description</label>
+                    <textarea class="form-control" id="inputDescription" rows="5"></textarea>
+                </div>
+                <button type="submit" class="btn-u btn-u-blue">Post</button>
+            </form>
+        </div>
+    </div>
 </div>
+
