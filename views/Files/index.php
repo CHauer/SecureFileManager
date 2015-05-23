14def37a88f2b94d6949eb66c44ff8e69bbc176c
MaHaller <77748@fhwn.ac.at>
2015-05-23 14:02:02 +0200

    File upload

--- views/Files/index.php
+++ views/Files/index.php
@@ -11,44 +11,68 @@
 </div><!--/breadcrumbs-->
 <!--=== End Breadcrumbs ===-->
 
+<?
+if($viewModel->exists("model"))
+{
+    $model = $viewModel->get("model");
+}
+?>
+
 <!--=== Content Part ===-->
 <div class="container content">
-
-    <?
-    if(IsPremiumUser()) {
+    <div class="row">
+        <?
+        if(IsPremiumUser()) {
+            ?>
+            <div class="md-margin-bottom-20">
+                <div class="margin-left-5">
+                    <h4>Upload a file</h4>
+                    <p><a class="color-green" href="/files/upload">click here</a> to upload a new file.</p>
+                </div>
+            </div>
+        <?
+        }
         ?>
-        <div class="row">
-            <div class="md-margin-bottom-20 margin-left-5">
-                <h4>Upload a file</h4>
-                <p><a class="color-green" href="/files/upload">click here</a> to upload a new file.</p>
-            </div>
-        </div>
-    <?
-    }
-    ?>
 
-    <div class="row md-margin-bottom-20">
-        <div class="job-img-inputs">
-            <!-- Serach -->
-            <div class="col-sm-4 md-margin-bottom-10">
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="fa fa-tag"></i></span>
-                    <input type="text" placeholder="What file are you looking for?" class="form-control">
+        <div class="md-margin-bottom-10">
+            <div class="job-img-inputs">
+                <!-- Serach -->
+                <div class="col-sm-4 md-margin-bottom-10">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-tag"></i></span>
+                        <input type="text" placeholder="What file are you looking for?" class="form-control">
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-4 md-margin-bottom-10">
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="fa fa-tag"></i></span>
-                    <input type="text" placeholder="Which user are you looking for?" class="form-control">
+                <div class="col-sm-4 md-margin-bottom-10">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-tag"></i></span>
+                        <input type="text" placeholder="Which user are you looking for?" class="form-control">
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-3">
-                <button type="button" class="btn-u btn-block btn-u-dark"> Search Files</button>
+                <div class="col-sm-3">
+                    <button type="button" class="btn-u btn-block btn-u-dark"> Search Files</button>
+                </div>
             </div>
         </div>
+
+        <div class="md-margin-bottom-20">
+            <div>
+                <select name="Count">
+                    <option value="All">All</option>
+                    <option value="20">20</option>
+                    <option value="10" selected>10</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+            <div>
+                <select name="Order">
+                    <option value="User">User Name</option>
+                    <option value="Date" selected>Upload Date</option>
+                </select>
+            </div>
+        </div>
+
     </div>
-
-
     <div class="row"></div>
 </div><!--/container-->
 <!--=== End Content Part ===-->
