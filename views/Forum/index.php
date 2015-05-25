ee3b1a091203fccf305631c20916954d4c0074bb
doleeb <dominik.leeb@gmx.at>
2015-05-25 17:26:43 +0200

    Anzeigen aller Threads in forum/index

--- views/Forum/index.php
+++ views/Forum/index.php
@@ -11,7 +11,59 @@
     }
     ?>
 
+    <?
+    $threads = null;
+
+    if($viewModel->exists("threads"))
+    {
+        $files = $viewModel->get("threads");
+    }
+    ?>
+
     <h1>Forum!</h1>
+    <div class="panel panel-yellow margin-bottom-40">
+        <div class="panel-heading
+            <h3 class="panel-title"><i class="fa fa-tasks"></i> Threads</h3>
+        </div>
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Created</th>
+                <th>Answers</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?
+            if (!is_null($threads)) {
+                foreach ($threads as $data) {
+                    ?>
+                    <tr>
+                        <td><? echo $data["Title"] ?></td>
+                        <td><? echo $data["Description"] ?></td>
+                        <td><? echo $data["Created"] ?></td>
+                        <td><? echo $data["EntryCount"] ?></td>
+                    </tr>
+                <?
+                }
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
+
+
+
+
+
+
+
+
+
+    <?
+
+    ?>
     <p>A forum is  gonna be here soon!</p>
 
     <div class="row md-margin-bottom-30">
