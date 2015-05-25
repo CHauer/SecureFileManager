e72abbdd70312976da566a1cb047db871f2c1b29
MaHaller <77748@fhwn.ac.at>
2015-05-25 17:27:31 +0200

    File upload

--- views/Files/delete.php
+++ views/Files/delete.php
@@ -22,7 +22,7 @@ if($viewModel->exists("model"))
 <div class="container content">
     <div class="row">
         <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-            <form class="reg-page" action="/files/delete/<?php echo ModelValue($model, 'UserFileId')?>" method="post">
+            <form class="reg-page" action="/files/delete/<?php echo $model->UserFileId ?>" method="post">
                 <? CreateHiddenAntiCSRFTokenField(); ?>
 
                 <div class="reg-header">
