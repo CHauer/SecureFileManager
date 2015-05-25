13d2a5955fcaf478f4b6c1a920960127556f5626
MaHaller <77748@fhwn.ac.at>
2015-05-25 21:58:39 +0200

    File download

--- views/Files/details.php
+++ views/Files/details.php
@@ -68,8 +68,8 @@ if($viewModel->exists("comments"))
 
                                     <div class="media-body">
                                         <h4 class="media-heading">
-                                            <strong><?php echo $data['Username'] ?></strong>
-                                            <small class="col-md-5"><?php echo ModelDateTimeValue($data['Created']) ?></small>
+                                            <strong><?php echo $data['Username'] ?>, </strong>
+                                            <small><?php echo ModelDateTimeValue($data['Created']) ?></small>
                                         </h4>
                                         <p style="word-wrap: break-word"><?php echo $data['Message'] ?></p>
                                     </div>
