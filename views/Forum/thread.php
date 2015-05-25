abb38248115a0d595639e84955af68b59524fd49
doleeb <dominik.leeb@gmx.at>
2015-05-25 16:39:17 +0200

    Anzeigen eines Threads anhand der ThreadId

--- views/Forum/thread.php
+++ views/Forum/thread.php
@@ -12,12 +12,9 @@
     if($viewModel->exists("error")){
         echo '<h3 class="color-red">' . $viewModel->get("error") . '</h3>';
     } else {
-    ?>
-
-    <h1>Showing thread!</h1>
-        <?
         if(!is_null($thread)) {
-            echo "<p>" . $thread->Title . "</p>";
+            echo "<h1>" . $thread->Title ."</h1>";
+            echo "<p>" . $thread->Description . "</p>";
         }
     }
     ?>
