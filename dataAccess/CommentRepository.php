ecb1c48d6f2f49740a77e71769c18ce11c62479d
MaHaller <77748@fhwn.ac.at>
2015-05-25 23:58:32 +0200

    file comment

--- dataAccess/CommentRepository.php
+++ dataAccess/CommentRepository.php
@@ -8,4 +8,52 @@
 
 class CommentRepository {
 
+    public function InsertComment(Comment $comment)
+    {
+        global $db;
+        $stmt = $db->prepare("INSERT INTO [dbo].[Comment]
+          ([Message],
+          [UserId],
+          [UserFile_UserFileId]
+     VALUES
+           (:Message,
+            :UserId,
+            :UserFileId)");
+
+        $stmt->bindParam(":Message", PrepareHtml($comment->Message));
+        $stmt->bindParam(":UserId", $comment->UserId);
+        $stmt->bindParam(":UserFileId", $comment->UserFile_UserFileId);
+
+        $stmt->execute();
+
+        if ($stmt->rowCount() == 1)
+        {
+            return $db->lastInsertId();
+        }
+
+        return false;
+    }
+
+    public function GetComments($fileid)
+    {
+        global $db;
+        $isprivate = 0;
+
+        $stmt = $db->prepare('Select [Comment].Message, [Comment].Created, Username, PictureLink
+                              From [Comment] left join [User] on [Comment].UserId = [User].UserId
+                              where [Comment].UserFile_UserFileId = :fileid
+                              order by Created DESC');
+
+        $stmt->bindParam(':fileid', $fileid);
+
+        $stmt->execute();
+
+        $results = $stmt->fetchAll();
+
+        if ($stmt->columnCount() >= 1) {
+            return $results;
+        }
+        return null;
+    }
+
 }
