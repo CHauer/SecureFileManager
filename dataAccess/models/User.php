79619ad2a7f8ad57b6dd649d713c6ed12ef40c7a
Christoph Hauer <hauer.christoph@live.at>
2015-05-28 01:34:16 +0200

    Create User.php

---
+++ dataAccess/models/User.php
@@ -0,0 +1,40 @@
+<?php
+
+class User
+{
+
+    public $UserId;
+
+    public $Username;
+
+    public $Firstname;
+
+    public $Lastname;
+
+    public $Password;
+
+    public $BirthDate;
+
+    public $EMail;
+
+    public $Description;
+
+    public $PictureLink;
+
+    public $LockoutEnabled;
+
+    public $LockoutEndDate;
+
+    public $AccessFailedCount;
+
+    public $Deactivated;
+
+    public $Role;
+
+    public $RoleId;
+
+    public $AuthToken;
+
+    public $AuthTokenId;
+
+}
