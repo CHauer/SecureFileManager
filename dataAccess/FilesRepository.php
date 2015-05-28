c3d9532ae9816736777154e9b159221425bd7ed0
MaHaller <77748@fhwn.ac.at>
2015-05-28 20:14:45 +0200

    My files

--- dataAccess/FilesRepository.php
+++ dataAccess/FilesRepository.php
@@ -163,13 +163,13 @@ class FileRepository
 
         $stmt = $db->prepare('Select [UserFile].*, Username, PictureLink,
                               (Select count(Commentid) From Comment where UserFile_UserFileId = [Userfile].UserFileId) as CommentCount
-                              from [UserFile] left join [User] on [UserFile].UserId = [User].UserId
-                              where Name LIKE :file and [UserFile].UserId = :id order by ' . $order . ', Name');
+                              from [UserFile] left join [User] on [UserFile].UserId = [User].UserId');
+                            //  where Name LIKE :file and [UserFile].UserId = :id order by ' . $order . ', Name');
 
         $file = '%' . $file . '%';
 
-        $stmt->bindParam(':id', $_SESSION["userid"]);
-        $stmt->bindParam(':file', $file, PDO::PARAM_STR);
+       // $stmt->bindParam(':id', $_SESSION["userid"]);
+        // $stmt->bindParam(':file', $file, PDO::PARAM_STR);
 
         $stmt->execute();
         $results = $stmt->fetchAll();
