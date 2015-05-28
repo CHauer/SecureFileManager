7b692c36a277f44d1f6bb2eca6f69df769680535
Christoph <hauer.christoph@live.at>
2015-05-28 02:30:42 +0200

    admin

--- views/Home/index.php
+++ views/Home/index.php
@@ -3,7 +3,7 @@
 <div class="purchase">
     <div class="container">
         <div class="row">
-            <div class="col-md-9 animated fadeInLeft">
+            <div class="col-md-6 animated fadeInLeft">
                 <span><?
                     if(!IsUserLoggedOn()) {
                         ?>
@@ -15,10 +15,12 @@
                     <?
                     }
                     ?></span>
-                <p></p>
             </div>
             <div class="col-md-3 btn-buy animated fadeInRight">
-                <a href="/account/login" class="btn-u btn-u-lg"><i class="fa fa-user"></i> Register</a>
+                <a href="/account/login" class="btn-u btn-u-lg"><i class="fa fa-user"></i> Login</a>
+            </div>
+            <div class="col-md-3 btn-buy animated fadeInRight">
+                <a href="/account/login" class="btn-u btn-u-lg"><i class="fa fa-bolt"></i> Register</a>
             </div>
         </div>
     </div>
@@ -28,7 +30,7 @@
 <!--=== Content Part ===-->
 <div class="container content">
     <div class="title-box">
-        <div class="title-box-text">We <span class="color-green">Do</span> Secure Files & Forum</div>
+        <div class="title-box-text">We <span class="color-green">Do</span> Secure!</div>
         <p></p>
     </div>
 
@@ -59,5 +61,29 @@
         </div>
     </div>
     <!-- End Info Blocks -->
+
+    <div class="row">
+        <div class="col-sm-4 info-blocks">
+            <i class="icon-info-blocks fa fa-upload"></i>
+            <div class="info-blocks-in">
+                <h3>Upload</h3>
+                <p>Upload Private, Public Filess</p>
+            </div>
+        </div>
+        <div class="col-sm-4 info-blocks">
+            <i class="icon-info-blocks fa fa-download"></i>
+            <div class="info-blocks-in">
+                <h3>Download</h3>
+                <p>Files from Users</p>
+            </div>
+        </div>
+        <div class="col-sm-4 info-blocks">
+            <i class="icon-info-blocks fa fa-database"></i>
+            <div class="info-blocks-in">
+                <h3>Threads</h3>
+                <p>Forum</p>
+            </div>
+        </div>
+    </div>
 
 </div>
