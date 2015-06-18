9df51ebbcb53b3d2140d677e5172d1d4f4ac99aa
Christoph <hauer.christoph@live.at>
2015-06-18 21:39:13 +0200

    Security Updates

--- views/Home/faq.php
+++ views/Home/faq.php
@@ -10,6 +10,6 @@
 
 <div class="container">
     <div class="row text-center margin-top-20">
-    <img src="/assets/img/Hello-IT-Have.jpg" class="col-md-12 col-xs-12" />
+    <img src="/assets/img/Hello-IT-Have.jpg" class="col-md-12 col-xs-12" style="margin-bottom: 30px;" />
         </div>
 </div>
