3bac253ee3d16482cb94626b23aa274792732687
Christoph Hauer <hauer.christoph@live.at>
2015-06-25 01:13:33 +0200

    Changes no global $log/$db

--- Includes/connectDB.php
+++ Includes/connectDB.php
@@ -2,15 +2,15 @@
 
 function CreateDatabaseAccess()
 {
-     $DB_NAME = 'SecureFileManagerDB';
-     $DB_USER = 'FHProjectsAdmin@vlpd8topfp';
-     $DB_PASSWORD = 'qwerASDF12';
-     $DB_HOST = 'tcp:vlpd8topfp.database.windows.net,1433';
+     $dbName = 'SecureFileManagerDB';
+     $dbUser = 'FHProjectsAdmin@vlpd8topfp';
+     $dbPassword = 'qwerASDF12';
+     $dbHost = 'tcp:vlpd8topfp.database.windows.net,1433';
 
     /* Connect*/
     try
     {
-        $db = new PDO("sqlsrv:server=" . $DB_HOST . ";Database=" . $DB_NAME, $DB_USER, $DB_PASSWORD);
+        $db = new PDO("sqlsrv:server=" . $dbHost . ";Database=" . $dbName, $dbUser, $dbPassword);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (Exception $e)
